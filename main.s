e9f6c67cd58f2cca630f74d42eb5751993fe7e0b
daltonbr <sacinopatinete@gmail.com>
2016-11-10 00:00:24 -0200

    Improvement in blinking mechanism [partially tested]

--- main.s
+++ main.s
@@ -44,9 +44,10 @@
 .equ RVALID_UART_mask,           0b1000000000000000  # more visual than 65536 [decimal]  
 
 # Constants
-.equ TIMER_INTERVAL,             0x017D7840          # 1/(50 MHz) × (0x17D7840) = 500 msec
+.equ RED_LED_TIMER_INTERVAL,     0x017D7840          # 1/(50 MHz) × (0x17D7840) = 500ms
 .equ ZERO_ASCII_VALUE,           0x30
-.equ ENTER_ASCII_VALUE,          0xd
+.equ ENTER_ASCII_VALUE,          0xa
+.equ DISPLAY_TIMER_INTERVAL,     0x989680            # 1/(50 MHz) x (0x989680) = 200ms
 
     .text                                                # executable code follows
 .org        0x20
@@ -65,37 +66,41 @@ TIMER_INTERRUPT:
 
     # Here, we are assuming that the only timer interrupts are regarding the blinking of leds
 
+    movia       r9, TIMER_BASE_ADDRESS
+    ldbio       r15, 0(r9)
+    andi        r15, r15, 0b11111110
+    stbio       r15, 0(r9)
+
     movia       r13, RED_LEDS_STATUS_ADDRESS         # Get buffer of blinkable leds
     movia       r8, RED_LED_BASE_ADDRESS
 
     # TODO: Toggle the needed bits in the RED_LED_BASE_ADDRESS
     ldwio       r10, 0(r8)
     ldw         r11, 0(r13)
-    beq         r10, 0, LIGHT_ALL_NEEDED_LEDS        # If its zero, there are no leds lighted on
+    beq         r10, r0, LIGHT_ALL_NEEDED_LEDS       # If its zero, there are no leds lighted on
 
-    xor         r12, r11, 0x11111111                 # Get the complement of the status address
-    and         r12, r12, r10                        # We AND here to reset the wanted bits on the status buffer
-    stwio       r12, 0(r8)                           # Writing the DATA (note: writing into this register
-    br RETURN_FROM_INTERRUPT                         # has no effect on received data)
+    stwio       r0, 0(r8)                            # tunr off all the freaking lights
+    br RETURN_FROM_INTERRUPT                         
 
 LIGHT_ALL_NEEDED_LEDS:
-    or          r10, r10, r13                        # Just light the needed lights
+    or          r10, r10, r11                        # Just light the needed lights
     stwio       r10, 0(r8)                           # Save it back to red leds address
     br RETURN_FROM_INTERRUPT
 
 # Reading the character
 UART_INTERRUPT:
-    ldbio       r10, 0(r8)                  # r10 = DATA (1 byte)
-    stb         r10, 0(r15)                 # store the read character into memory
-    addi        r15, r15, 1                 # Increment the pointer for the command
-    addi        r11, r0, ENTER_ASCII_VALUE  # r11 is a temp register
-    beq         r10, r11, VALIDATE_COMMAND  # Checks for <ENTER>
+    ldbio       r10, 0(r8)                           # r10 = DATA (1 byte)
+    stb         r10, 0(r15)                          # store the read character into memory
+    addi        r15, r15, 1                          # Increment the pointer for the command
+    addi        r11, r0, ENTER_ASCII_VALUE           # r11 is a temp register
+    beq         r10, r11, VALIDATE_COMMAND           # Checks for <ENTER>
     br RETURN_FROM_INTERRUPT
 
 VALIDATE_COMMAND:
     movia       r15, COMMAND_BASE_ADDRESS         # reset the command pointer to the base address
     ldb         r10, 0(r15)                       # Load the first character typed
     addi        r15, r15, 1                       # Increment the pointer
+    subi        r10, r10, ZERO_ASCII_VALUE        # we want the numeric value
     beq         r10, r0, ZERO_PREFIX_COMMAND      # Check for 0
     addi        r11, r0, 1
     beq         r10, r11, ONE_PREFIX_COMMAND      # Check for 1
@@ -107,6 +112,7 @@ VALIDATE_COMMAND:
 ZERO_PREFIX_COMMAND:
     ldb         r10, 0(r15)                       # Load the second character typed
     addi        r15, r15, 1                       # Increment the pointer
+    subi        r10, r10, ZERO_ASCII_VALUE        # we want the numeric value
     beq         r10, r0, ZERO_ZERO_COMMAND        # Check for 0
     addi        r11, r0, 1
     beq         r10, r11, ZERO_ONE_COMMAND        # Check for 1
@@ -116,6 +122,7 @@ ZERO_PREFIX_COMMAND:
 ONE_PREFIX_COMMAND:
     ldb         r10, 0(r15)                       # Load the second character typed
     addi        r15, r15, 1                       # Increment the pointer
+    subi        r10, r10, ZERO_ASCII_VALUE        # we want the numeric value
     beq         r10, r0, ONE_ZERO_COMMAND         # Check for 0
     movia       r15, COMMAND_BASE_ADDRESS         # Invalid command
     br RETURN_FROM_INTERRUPT    
@@ -123,6 +130,7 @@ ONE_PREFIX_COMMAND:
 TWO_PREFIX_COMMAND:
     ldb         r10, 0(r15)                       # Load the second character typed
     addi        r15, r15, 1                       # Increment the pointer
+    subi        r10, r10, ZERO_ASCII_VALUE        # we want the numeric value
     beq         r10, r0, TWO_ZERO_COMMAND         # Check for 0
     addi        r11, r0, 1
     beq         r10, r11, TWO_ONE_COMMAND         # Check for 1
@@ -177,13 +185,20 @@ BLINK_RED_LEDS:
     movia       r8, RED_LED_BASE_ADDRESS
     movia       r9, RED_LEDS_STATUS_ADDRESS
 
-    ldwio       r10, 0(r8)                     # r10 = current value in RLED Data Register
-    or          r12, r11, r10                  # the user input need to be ORed with the current value in RLEDs
-    stwio       r12, 0(r8)                     # set the RLED Data Register
-
     ldw         r10, 0(r9)                     # r10 = current value in the status red leds memory
-    or          r12, r11, r1                   # the user input need to be ORed with the current status value
-    stw         r11, 0(r9)                     # Set the red leds status
+    or          r10, r11, r10                  # the user input need to be ORed with the current status value
+    stw         r10, 0(r9)                     # Set the red leds status
+
+    # set the interval timer period for the red leds
+    movia       r9, TIMER_BASE_ADDRESS
+    movia       r12, RED_LED_TIMER_INTERVAL    # 1/(50 MHz) × (0x17D7840) = 500 msec
+    sthio       r12, 8(r9)                     # store the low halfword of counter (low)...
+    srli        r12, r12, 16                   # move the high halfword to the low part
+    sthio       r12, 0xC(r9)                   # ...and then store it in the the counter (high)
+
+    # start interval timer, enable its interrupts and set it to reload when reach 0
+    movi        r12, 0b0111                    # START = 1, CONT = 1, ITO = 1
+    sthio       r12, 4(r9)
 
     br RETURN_FROM_INTERRUPT
 
@@ -192,10 +207,12 @@ CANCEL_BLINK_RED_LEDS:
     movia       r9, RED_LEDS_STATUS_ADDRESS
 
     # Reset the bit on the status buffer
-    lbw         r11, 0(r9)                     # Load current status buffer
-    xor         r12, r10, 0x11111111           # Get the complement of user input
+    ldw         r11, 0(r9)                     # Load current status buffer
+    xori        r12, r10, 0b11111111           # Get the complement of user input
     and         r12, r12, r11                  # We AND here to reset the wanted bit on the status buffer
     stw         r12, 0(r9)                     # Store it into memory again
+
+    # TODO: If there are no bits left to light, stop the timer
 
 RETURN_FROM_INTERRUPT:
     subi        ea, ea, 4                   # external interrupt must decrement ea, so that the 
@@ -209,27 +226,26 @@ _start:
 
     movia       r8, UART_BASE_ADDRESS
     movia       r9, TIMER_BASE_ADDRESS
+    movia       r10, RED_LEDS_STATUS_ADDRESS
     movia       r15, COMMAND_BASE_ADDRESS
 
+    stw         r0, 0(r10)                  # Reset the red leds status address
+
     # enable Nios II processor interrupts
-    movi        r7, 0b100000000             # set interrupt mask bits for
+    movi        r7, 0b100000001             # set interrupt mask bits for
     wrctl       ienable, r7                 # and #8 (JTAG port)
     movi        r7, 1
     wrctl       status, r7                  # turn on Nios II interrupt processing
 
     # TODO: print the prompt message
 
+    # enable JTAG uART interrupt for reading (RE)
+
+    ldbio       r12, 4(r8)
+    ori         r12, r12, 0b00000001
+    sthio       r12, 4(r8)
+
     IDLE:
         br IDLE
-
-# /* set the interval timer period for scrolling the HEX displays */
-#     movia       r12, TIMER_INTERVAL         # 1/(50 MHz) × (0x17D7840) = 500 msec
-#     sthio       r12, 8(r9)                  # store the low halfword of counter (low)...
-#     srli        r12, r12, 16                # move the high halfword to the low part
-#     sthio       r12, 0xC(r9)                # ...and then store it in the the counter (high)
-
-# /* start interval timer, enable its interrupts and set it to reload when reach 0 */
-#     movi        r13, 0b0111                 # START = 1, CONT = 1, ITO = 1
-#     sthio       r13, 4(r9)
     
 .end
